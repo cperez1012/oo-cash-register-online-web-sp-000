dbfeb53ad0ffbe89827f8b0dbed721439c409c92
Christian Perez <perezchristian1012@gmail.com>
2019-08-22 21:10:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+require 'pry'
+class CashRegister
+  attr_accessor :total, :employee_discount, :items
 
+  def initialize(employee_discount = nil)
+    @total = 0.00
+    @employee_discount = employee_discount
+    @items = []
+  end
+
+  def discount
+    self.employee_discount
+  end
+
+  def items
+    @items
+  end
+end
